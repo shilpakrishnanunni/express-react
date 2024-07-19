247055344da664a1f69a2e20ec622d4641179953
ShilpaKrishnanUnni <shilpakrishnan@teamioss.in>
2024-07-19 15:40:30 +0530

    backend: add new routes under /budget

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,7 @@
 import homeRoutes from "./routes/home.route.js";
+import budgetRoutes from "./routes/budget.route.js";
 
 export default function mountRoutes(app) {
-    app.use("/home", homeRoutes)
+    app.use("/home", homeRoutes),
+    app.use("/budget", budgetRoutes)
 }
