e688560f36eec31ce70ceed47df153f064b7776c
ShilpaKrishnanUnni <shilpakrishnan@teamioss.in>
2024-07-29 10:58:11 +0530

    backend(mod): update the getBudgetDashboard function to split categories for dropdown and list. add monitoring middleware in app.js

--- server/controllers/budget.controller.js
+++ server/controllers/budget.controller.js
@@ -4,18 +4,26 @@ import Users from "../models/users.model.js";
 
 export const getBudgetDashboard = async (req, res, next) => {
     try {
-        console.log("-----getBudgetDashboard----")
         const userID = 1;
         const response = {};
         const [budgetCategories, user] = await Promise.all([
             Categories.findAll({ attributes: ["id", "name", "status", "recurring"], raw:true }),
             Users.findOne({ attributes: ["budget"], where: { id: userID }, raw: true })
-        ])
-
-        response.budgetCategories = budgetCategories;
+        ]);
         response.budgetAmount = user.budget;
-        console.log(response)
-        return res.json({ response });
+        
+        let dropdownCategories; let listCategories;
+        if (budgetCategories.length==0) {
+            dropdownCategories = { value: "", label: "No Categories" };
+            listCategories = {};
+            return res.json({ budgetAmount: user.budget, budgetCategories: { dropdownCategories, listCategories } });
+        }
+
+        dropdownCategories = budgetCategories.filter((category) => category.status == '0').map (row => ({ value: row.id, label: row.name }));
+        dropdownCategories.push({ value: '', label: 'SELECT CATEGORY' });
+        listCategories = budgetCategories.filter((category) => category.status == '1');
+        
+        return res.json({ budgetAmount: user.budget, budgetCategories: { dropdownCategories, listCategories } });
     } catch (error) {
         console.log("ERROR FROM getBudgetDashboard", error);
         return next(error);        
@@ -24,8 +32,6 @@ export const getBudgetDashboard = async (req, res, next) => {
 
 export const addCategory = async (req, res, next) => {
     try {
-        console.log(`-----${req.method} ${req.url}-----`);
-        console.log(req.body)
         const { category, recurring } = req.body;
         if (!category || !recurring) return res.status(500).json("INVALID VALUES");
     
@@ -34,7 +40,7 @@ export const addCategory = async (req, res, next) => {
             status: "0",
             recurring: req.body.recurring ? "1" : "0"
         })
-        return res.status(200);
+        return res.status(200).json({ success: true });
     } catch (error) {
         console.log("ERROR FROM addCategory", error);
         return next(error);
@@ -43,12 +49,10 @@ export const addCategory = async (req, res, next) => {
 
 export const selectCategory = async (req, res, next) => {
     try {
-        console.log(`-----${req.method} ${req.url}-----`);
-        console.log(req.body)
         const { categoryId } = req.body;
     
         await Categories.update({ status: "1" }, { where: { id: categoryId } });
-        return res.status(200);
+        return res.status(200).json({ success: true });
     } catch (error) {
         console.log("ERROR FROM selectCategory", error);
         return next(error);
@@ -59,7 +63,7 @@ export const alterBudget = async (req, res, next) => {
     try {
         const userID = 1;
         await Users.update({ budget: req.body.budget }, { where: { id: userID } });
-        return res.status(200);
+        return res.status(200).json({ success: true });
     } catch (error) {
         logger.error("ERROR FROM alterBudget", error);
         return next(error);
