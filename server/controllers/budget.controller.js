06110bd08412f6348cfe255e7aafbf9e90d4e6ce
ShilpaKrishnanUnni <shilpakrishnan@teamioss.in>
2024-07-29 15:02:35 +0530

    backend(mod): use MonthlyBudget table to track a particular user's budget categories.

--- server/controllers/budget.controller.js
+++ server/controllers/budget.controller.js
@@ -1,15 +1,24 @@
-import Categories from "../models/categories.model.js";
-import Users from "../models/users.model.js";
-
+import { Categories, MonthlyBudget, Users } from "../models/index.js";
 
 export const getBudgetDashboard = async (req, res, next) => {
     try {
         const userID = 1;
         const response = {};
-        const [budgetCategories, user] = await Promise.all([
-            Categories.findAll({ attributes: ["id", "name", "status", "recurring"], raw:true }),
-            Users.findOne({ attributes: ["budget"], where: { id: userID }, raw: true })
+        const [budgetCategories, user, monthlyBudget] = await Promise.all([
+            Categories.findAll({ attributes: ["id", "name"], where: { status: '1' }, raw:true }),
+            Users.findOne({ attributes: ["budget"], where: { id: userID }, raw: true }),
+            MonthlyBudget.findAll({
+                where: { userId: 1 },
+                include: [
+                    {
+                        model: Categories,
+                        attributes: ["id", "name", "recurring"]
+                    }
+                ],
+                raw: true
+            })
         ]);
+        console.log("budgetCategories",budgetCategories)
         response.budgetAmount = user.budget;
         
         let dropdownCategories; let listCategories;
@@ -19,9 +28,15 @@ export const getBudgetDashboard = async (req, res, next) => {
             return res.json({ budgetAmount: user.budget, budgetCategories: { dropdownCategories, listCategories } });
         }
 
-        dropdownCategories = budgetCategories.filter((category) => category.status == '0').map (row => ({ value: row.id, label: row.name }));
+        dropdownCategories = budgetCategories.map (row => ({ value: row.id, label: row.name }));
         dropdownCategories.push({ value: '', label: 'SELECT CATEGORY' });
-        listCategories = budgetCategories.filter((category) => category.status == '1');
+        listCategories = monthlyBudget.map(row => ({
+            id: row.id,
+            name: row['Category.name'],
+            totalAmount: row.totalAmount,
+            amountLeft: row.amountLeft,
+            recurring: row['Category.recurring']
+        }));
         
         return res.json({ budgetAmount: user.budget, budgetCategories: { dropdownCategories, listCategories } });
     } catch (error) {
@@ -29,6 +44,9 @@ export const getBudgetDashboard = async (req, res, next) => {
         return next(error);        
     }
 };
+
+// TODO check recurring -> Categories.recurring = 1 where userId
+// TODO BudgetHistory table -> user_id, budget(amount), monthstart, monthend
 
 export const addCategory = async (req, res, next) => {
     try {
@@ -51,7 +69,13 @@ export const selectCategory = async (req, res, next) => {
     try {
         const { categoryId } = req.body;
     
-        await Categories.update({ status: "1" }, { where: { id: categoryId } });
+        // await Categories.update({ status: "1" }, { where: { id: categoryId } });
+        await MonthlyBudget.create({
+            userId: 1,
+            categoryId,
+            totalAmount: 0,
+            amountLeft: 0
+        });
         return res.status(200).json({ success: true });
     } catch (error) {
         console.log("ERROR FROM selectCategory", error);
