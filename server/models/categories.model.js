faa1c21fe6cfd63963a11da8d4dccb821c322c45
ShilpaKrishnanUnni <shilpakrishnan@teamioss.in>
2024-08-08 09:45:25 +0530

    backend(mod): categories model new field - userId

--- server/models/categories.model.js
+++ server/models/categories.model.js
@@ -3,9 +3,11 @@ import sequelize from "../config/db.js";
 
 const Categories = sequelize.define("Categories",
     {
+        userId: DataTypes.STRING(255),
         name: DataTypes.STRING(255),
         status: { type: DataTypes.ENUM, values: ["0", "1"] },
         recurring: { type: DataTypes.ENUM, values: ["0", "1"] },
+        // type : credit/debit
 
     }, { sequelize }
 );
